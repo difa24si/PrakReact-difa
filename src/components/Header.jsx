907da21bdf85d9d0eadfdfaf3c006e97e31bef1e
difa24si <difa24si@mahasiswa.pcr.ac.id>
2026-06-05 15:08:01 +0700

    ubah jadi Haii

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -57,7 +57,7 @@ export default function Header() {
         {/* USER */}
         <div className="flex items-center gap-2 ml-4">
           <span className="text-sm text-gray-600">
-            Hello, <b>Difa Mardiani</b>
+            Haii, <b>Difa Mardiani</b>
           </span>
           <img
             src="https://i.pravatar.cc/40"
